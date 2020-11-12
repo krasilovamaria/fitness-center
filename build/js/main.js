f1067d1de3a2c349360f82df6088e8c374789d0a
maria <marivasi11@yandex.ru>
2020-11-12 12:27:14 +0700

    Исправляет замечания

--- build/js/main.js
+++ build/js/main.js
@@ -1,44 +1,45 @@
 'use strict';
 
-var slider = document.querySelector('.trainers');
-slider.classList.remove('trainers--nojs');
-
-var commentsSlider = document.querySelector('.comments');
-commentsSlider.classList.remove('comments--nojs');
+var sliderTrainers = document.querySelector('.slider');
+var sliderComments = document.querySelector('.comments__slider');
 
 $(document).ready(function () {
-  $('.multiple-items').slick({
-    arrows: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    speed: 1000,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+  if (sliderTrainers.classList.contains('multiple-items')) {
+    $('.multiple-items').slick({
+      arrows: true,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      speed: 1000,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 2
+          }
         }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      }
-    ]
-  });
-  $('.comments__slider').slick({
-    arrows: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000
-  });
+      ]
+    });
+  }
+  if (sliderComments.classList.contains('multiple-comments')) {
+    $('.multiple-comments').slick({
+      arrows: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 1000
+    });
+  }
 });
 
 var linkNav = document.querySelectorAll('[href^="#"]');
